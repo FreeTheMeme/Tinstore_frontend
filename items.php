f3d9a108e2738b05e0e33f6c3bf33c00229b8e8a
bigtin <bigtin@protonmail.com>
2025-09-18 17:14:31 -0400

    items start

--- items.php
+++ items.php
@@ -1,4 +1,11 @@
 <?php
+    $apiUrl ="http://192.168.12.155:5000/items";
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $apiUrl);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $result = json_decode($response, true);
 ?>
 
 <!DOCTYPE html>
@@ -14,6 +21,8 @@
 <a href="items.php">items</a> |
 <a href="edit.php">edit</a>
    <h1>Items</h1> 
-
+        <?php
+            print_r($result)
+        ?>
 </body>
 </html>
